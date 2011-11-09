570eaa53b94039484f8ca0a47c7e98d4ec8ffaca
Alexey Osipenko <aratak@anahoret.com>
2011-11-09 08:21:05 +0200

    rails 3.1 changes

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,10 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
+# require 'rails/all'
+require 'action_controller/railtie'
+require 'active_resource/railtie'
+require 'sprockets/railtie'
 
 Bundler.require
 require "liqueur"
@@ -41,5 +40,7 @@ module Dummy
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.assets.enabled = true
   end
 end
